4816a372b8aae69e6e612516b36ab4decdbf9c35
guo <503392549@qq.com>
2019-04-23 21:08:33 +0800

    4.23 Cookie 购物车

---
+++ cookie.js
@@ -0,0 +1,15 @@
+function addCookie(key,value,days){
+    var now = new Date();
+    now.setTime(now.getDate() + days);
+    document.cookie = key+"="+value+";"+"expires="+now.getDate();
+}
+function getCookie(key){
+    //按分号分割每个键值
+    var list = document.cookie.split(";");
+    for (var i in list) {
+        // 分割键值的键和值
+        var kv = list[i].split("="); // kv临时变量，保存每一次分割后的键值对
+        if(kv[0] ==key )
+            return kv[1];
+    }
+}
