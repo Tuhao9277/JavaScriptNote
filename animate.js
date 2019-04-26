5c8756a0d60306ce4c05d5e0cf0d357caceb4f97
guo <503392549@qq.com>
2019-04-26 20:33:30 +0800

    bug修复

--- animate.js
+++ animate.js
@@ -29,7 +29,7 @@ function easyEaseMotion(ele, target,line,times=900) {
  * @param {Element} ele 节点名
  * @param {Object} props 目标
  */
-function sinMotion(ele,props,line,times=900){
+function sinMotion(ele,props,line,times=900,callback){
     // 循环获取目标所有的属性
     for (let attr in props) {
         // 获取属性的初值
@@ -46,72 +46,79 @@ function sinMotion(ele,props,line,times=900){
             // var p = ele.cloneNode(true);
             // line.appendChild(p);
           if(degree == 90){
-              clearInterval(t);
+			
+			  clearInterval(t);
+			  callback ? callback() : "";
           }
         },30)
-    }
+	}
+
 
 }
-function getStyle(ele){
-    if(window.VBArray){
-        return ele.getCurrentStyle;
-    }
-    return getComputedStyle(ele);
-}
-//animate( obj, {left: 300, width: 400})
-function animate(ele, options, callback){
-	if(ele.isMoving) return;
-	
-	ele.isMoving = true;
-	for(var attr in options) {
-		(function(prop){
-			var targetvalue = options[prop];  //获取到终点值
-			ele[prop+"-timer"] = setInterval(function(){
-				if(prop == "opacity") {
-					var currentValue = parseFloat(getStyle(ele)[prop])*100;
-					var speed = (targetvalue - currentValue)/7;
-					speed = speed>0?Math.ceil(speed):Math.floor(speed);
-					ele.style.opacity = (currentValue + speed)/100;
-					ele.style.filter = "alpha(opacity="+(currentValue + speed)+")"
-					if(ele.style.opacity == targetvalue/100) {
-						clearInterval(ele["opacity-timer"]);
-						if(isAllover()) {
-							callback ? callback() : "";
-						}
+//运动插件(通过三角函数)
+function animate(ele, options, callback) {
+
+	stopTimer();
+	//定义一个定时器列表,将它绑定在ele元素
+	ele.timerlist = [];
+
+	for (let attr in options) { //遍历得到每一个操作{left:300},attr是每一个属性名
+			//首先获得起始位置
+			let start = parseFloat(getStyle(ele)[attr]);
+		
+			//定义一个角度
+			let degree = 0;
+			//得到需要运动的距离
+			let distance = options[attr] - start;
+
+			//设置定时器
+			let t = setInterval(function () {
+					//将speed每次加3
+					degree += 3;
+					if (attr == "opacity") {
+							ele.style[attr] = Math.sin(Math.PI / 180 * degree) * distance + start
+					} else {
+							ele.style[attr] = Math.sin(Math.PI / 180 * degree) * distance + start + "px";
 					}
-				} else {
-					var currentValue = parseInt(getStyle(ele)[prop]);
-					var speed = (targetvalue - currentValue)/7;
-					speed = speed>0?Math.ceil(speed):Math.floor(speed);
-					ele.style[prop] =  currentValue + speed + "px";
-					
-				}
-				
-				if(parseInt(getStyle(ele)[prop]) == targetvalue) {
-					clearInterval(ele[prop+"-timer"]);
-					if(isAllover()) {
-						callback ? callback() : "";
+
+					//设置终止条件
+					if (degree == 90) {
+							clearInterval(t);
+							//取得这个定时器的下标
+							var index = ele.timerlist.indexOf(t);
+							//将其剔除
+							ele.timerlist.splice(index, 1);
+							//如果数组的长度为0就让他回调,即这个option里面的所有操作都执行完了,就开始执行回调函数
+							ele.timerlist.length == 0 ? (callback ? callback() : "") : "";
 					}
-				}
 			}, 30);
-			
-		})(attr);
+			ele.timerlist.push(t);
 	}
-	
-	function isAllover() {
-		var flag = true;
-		for(var attr in options) {
-			var targetval = options[attr];
-			var curtVal = parseInt(getStyle(ele)[attr]);
-			if(attr == "opacity") {
-				curtVal = getStyle(ele)[attr]*100;
+
+	function getStyle(ele) {
+			if (window.VBArray) {
+					return ele.currentStyle;
+			} else {
+					return getComputedStyle(ele);
 			}
-			if(curtVal != targetval) {
-				flag = false;
-				return flag;
-			}
-		}
-		ele.isMoving = false;
-		return flag;
 	}
+	//刚进来这个函数,如果发现之前执行过该函数,那么直接给他清空
+	function stopTimer() {
+			//如果这个数组为空就直接返回
+			if (!ele.timerlist) return;
+			var t = null; //定义一个变量
+			while (t = ele.timerlist.pop()) {
+					//因为pop返回值是长度,所以只能为0的时候,才会不执行了
+					clearInterval(t);
+			}
+	}
+
 }
+
+// function getStyle(ele) {
+//     if (window.VBArray) {
+//         return ele.srcElement;
+//     } else {
+//         return getComputedStyle(ele);
+//     }
+// }
